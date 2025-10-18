2734ae1e2f82d4aa461224e72631b4a9436bb39a
Dendi Ricaldi <Rivaldydendy459@Gmail.Com>
2025-10-18 22:42:40 +0800

    Uncomment all routes and restore original homepage

--- server.js
+++ server.js
@@ -1,36 +1,30 @@
-const express = require('express');
+﻿const express = require('express');
 const app = express();
 const port = 3000;
 
-// Only Dendi's work: User Management + Database + Unit Testing
+const aboutUsRoutes = require('./routes/aboutUsRoutes');
+const greetingRoutes = require('./routes/greetingRoutes');
 const userRoutes = require('./routes/userRoutes');
-
-// Other team members' routes (commented out)
-// const aboutUsRoutes = require('./routes/aboutUsRoutes');
-// const greetingRoutes = require('./routes/greetingRoutes');
-// const productsRoutes = require('./routes/productsRoutes');
-// const cartRoutes = require('./routes/cartRoutes');
+const productsRoutes = require('./routes/productsRoutes');
+const cartRoutes = require('./routes/cartRoutes');
 
 // Serve static demo UI
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.redirect('/user-management-demo.html');
+  res.send('<h1>Welcome to the homepage with Express!</h1><p><a href="/aboutus">About Us (JSON)</a> | <a href="/index.html">Demo UI</a></p>');
 });
 
-// Only Dendi's user management routes
+app.use('/aboutus', aboutUsRoutes);
+app.use('/greeting', greetingRoutes);
 app.use('/users', userRoutes);
-
-// Other team members' routes (commented out)
-// app.use('/aboutus', aboutUsRoutes);
-// app.use('/greeting', greetingRoutes);
-// app.use('/products', productsRoutes);
-// app.use('/cart', cartRoutes);
+app.use('/products', productsRoutes);
+app.use('/cart', cartRoutes);
 
 app.use((req, res) => {
   res.status(404).send('404 Not Found');
 });
 
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+  console.log(Server running on http://localhost:);
 });
